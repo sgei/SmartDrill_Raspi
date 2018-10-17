96cb51b1ab284c4f85fcd2b45eefe86088761341
Steffen Geier <SGeier@ad.intershop.net>
2018-10-17 16:44:22 +0200

    add nodejs-files

--- home/pi/intershop/scripts/drill/reset.sh
+++ home/pi/intershop/scripts/drill/reset.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ls #!/bin/bash
 
 PROG=`basename $0`								# Program-Name
 DRILL_PATH=/home/pi/intershop/scripts/drill		# Path for main-script and config-file
@@ -15,13 +15,13 @@ do
 
 	lxterminal --working-directory=$DRILL_PATH --command="python drill.py"
 
-	#sleep 15
-	#lxterminal -e "testrpc --account=\"0x645a1e7f7d16658f2fb625b25956ed85fe4b3f25e768b99e480249de150e5b8e, 60000000000000000000\" --account=\"0x885d562b23803d62eed698530e18bef8d71b6d9bd535b56bf4a44ff1928fee1e, 0\""
+	sleep 5
+	lxterminal --working-directory=$DRILL_PATH --command="ganache-cli --account=\"0x645a1e7f7d16658f2fb625b25956ed85fe4b3f25e768b99e480249de150e5b8e, 60000000000000000000\" --account=\"0x885d562b23803d62eed698530e18bef8d71b6d9bd535b56bf4a44ff1928fee1e, 0\""
 
-	#sleep 15
-	#lxterminal -e "node $DRILL_PATH/index.js"
+	sleep 15
+	lxterminal --working-directory=$DRILL_PATH --command="node $DRILL_PATH/index.js"
 
-	#sleep 10
+	sleep 10
 
 	while [ $RESET -eq $TEMP1 ]
 	do
@@ -33,7 +33,7 @@ do
 	# kill drill.py
 	kill `ps -aux | grep -a -e "drill.py" | grep -a -v "grep" | tr -s " " | cut -d " " -f2`
 	# kill TESTRPC
-	#kill `ps -aux | grep -a -e "testrpc" | grep -a -v "grep" | tr -s " " | cut -d " " -f2`
+	kill `ps -aux | grep -a -e "ganache-cli" | grep -a -v "grep" | tr -s " " | cut -d " " -f2`
 
 	sleep 5
 done
--- setup_smartdrill.sh
+++ setup_smartdrill.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+IPATH="/home/pi/intershop"
 SPATH="/home/pi/intershop/scripts"
+MODULE=""
 
 clear
 
@@ -24,10 +26,10 @@ sudo systemctl start shutdown.service
 echo -e "Done..."
 
 echo -e "\n### Checking for azure-iothub-device-client..."
-azure=""
-azure=`sudo pip freeze | grep -a -e "azure-iothub-device-client"`
+MODULE=""
+MODULE=`sudo pip freeze | grep -a -e "azure-iothub-device-client"`
 
-if [ -z "$azure" ]; then
+if [ -z "$MODULE" ]; then
 	echo "-> not installed"
 	echo "-> install package..."
 	sudo pip install --exists-action a azure-iothub-device-client
@@ -37,10 +39,10 @@ fi
 echo -e "Done..."
 
 echo -e "\n### Checking for libboost-python-dev..."
-libboost=""
-libboost=`sudo dpkg -l | grep -a -e "libboost-python-dev"`
+MODULE=""
+MODULE=`sudo dpkg -l | grep -a -e "libboost-python-dev"`
 
-if [ -z "$libboost" ]; then
+if [ -z "$MODULE" ]; then
 	echo "-> not installed"
 	echo "-> install package..."
 	sudo apt-get -qy install libboost-python-dev
@@ -50,10 +52,10 @@ fi
 echo -e "Done..."
 
 echo -e "\n### Checking for inotify..."
-inotify=""
-inotify=`sudo pip freeze | grep -a -e "inotify" | grep -a -v "pyinotify"`
+MODULE=""
+MODULE=`sudo pip freeze | grep -a -e "inotify" | grep -a -v "pyinotify"`
 
-if [ -z "$inotify" ]; then
+if [ -z "$MODULE" ]; then
 	echo "-> not installed"
 	echo "-> install package..."
 	sudo pip install --exists-action a inotify
@@ -63,16 +65,112 @@ fi
 echo -e "Done..."
 
 echo -e "\n### Install autostart script..."
-autostart=""
-autostart=`cat /home/pi/.config/lxsession/LXDE-pi/autostart | grep -a -e "autostart.sh"`
+MODULE=""
+MODULE=`cat /home/pi/.config/lxsession/LXDE-pi/autostart | grep -a -e "autostart.sh"`
 
-if [ -z "$autostart" ]; then
+if [ -z "$MODULE" ]; then
 	echo "-> autostart not configured"
 	echo "-> configure autostart..."
 	echo -e "\n\n# Intershop SmartDrill for Raspi" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 	echo "@/home/pi/intershop/scripts/drill/autostart.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 else
 	echo "-> autostart already configures"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for nodejs"
+MODULE=""
+MODULE=`sudo dpkg -l | grep -a -e "nodejs"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	sudo apt-get -qy install nodejs
+else
+	echo "-> package already installed"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for npm"
+MODULE=""
+MODULE=`sudo dpkg -l | grep -a -e "npm"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	sudo apt-get -qy install npm
+else
+	echo "-> package already installed"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for web3"
+MODULE=""
+MODULE=`sudo npm list --depth=0 | grep -a -e "web3"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	cd SPATH/drill
+	npm install -g web3
+else
+	echo "-> package already installed"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for golang"
+MODULE=""
+MODULE=`sudo dpkg -l | grep -a -e "golang"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	sudo apt-get -yq install golang
+else
+	echo "-> package already installed"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for geth"
+MODULE=""
+MODULE=`sudo ls -l /usr/local/bin | grep -ae "geth"`
+ARM7=`cat /proc/cpuinfo | grep -ae "ARMv7"`
+ARM6=`cat /proc/cpuinfo | grep -ae "ARMv6"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	cd $IPATH
+	if [ -n "ARM7" ]; then
+		wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm7-1.8.17-8bbe7207.tar.gz
+		tar -xvzf geth-linux-arm7-1.8.17-8bbe7207.tar.gz
+		sudo mv geth-linux-arm7-1.8.17-8bbe7207/geth /usr/local/bin
+		sudo chown root:staff /usr/local/bin/geth
+		rm -rf geth-linux-arm7-1.8.17-8bbe7207*
+	fi
+	if [ -n "ARM6" ]; then
+		wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm6-1.8.17-8bbe7207.tar.gz
+		tar -xvzf geth-linux-arm6-1.8.17-8bbe7207.tar.gz
+		sudo mv geth-linux-arm6-1.8.17-8bbe7207/geth /usr/local/bin
+		sudo chown root:staff /usr/local/bin/geth
+		rm -rf geth-linux-arm6-1.8.17-8bbe7207*
+	fi
+else
+	echo "-> package already installed"
+fi
+echo -e "Done..."
+
+echo -e "\n### Checking for ganache-cli"
+MODULE=""
+MODULE=`sudo npm list --depth=0 | grep -a -e "ganache-cli"`
+
+if [ -z "$MODULE" ]; then
+	echo "-> not installed"
+	echo "-> install package..."
+	cd $SPATH/drill
+	npm install -g ganache-cli
+else
+	echo "-> package already installed"
 fi
 echo -e "Done..."
 
